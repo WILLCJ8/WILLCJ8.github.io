88fc39d4c3a0fb0f44f6a9ffda3cb8d4b070f389
WILLCJ8 <50220258+WILLCJ8@users.noreply.github.com>
2020-04-04 15:40:29 +1300

    Added and should work json

--- Javascript.js
+++ Javascript.js
@@ -1,2 +1,10 @@
 let myObj = {"myKey":"some kind of value"};
 console.log(myObj["myKey"]); //will show "some kind of value"
+
+fetch('data.json')
+.then(res => {
+    return res.json();
+})
+.then(data => {
+    console.log(data);
+});
