43beb09924ef5365f4110f327ab1fbdf91fa4bf2
WILLCJ8 <50220258+WILLCJ8@users.noreply.github.com>
2020-04-09 22:08:57 +1200

    New addition/fix - 09/04/2020

--- Javascript.js
+++ Javascript.js
@@ -1,7 +1,7 @@
 let myObj = {"myKey":"some kind of value"};
 console.log(myObj["myKey"]); //will show "some kind of value"
 
-fetch('data.json')
+fetch('./data.json')
 .then(res => {
     return res.json();
 })
