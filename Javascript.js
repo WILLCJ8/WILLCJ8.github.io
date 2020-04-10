b9463f4a05372a9dffafdcaf2cd679dd75389544
WILLCJ8 <50220258+WILLCJ8@users.noreply.github.com>
2020-04-10 19:14:37 +1200

    Added comment form and contact form - need to add time recording to comment form - 10/04/2020

--- Javascript.js
+++ Javascript.js
@@ -1,6 +1,10 @@
 let myObj = {"myKey":"some kind of value"};
 console.log(myObj["myKey"]); //will show "some kind of value"
 
+function submitAlert() {
+    alert("Thank you for your message.");
+  }
+
 fetch('./data.json')
 .then(res => {
     return res.json();
