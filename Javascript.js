94afa04b3e88e2e926d9db0b81f8e99a0d58647b
WILLCJ8 <50220258+WILLCJ8@users.noreply.github.com>
2020-04-29 18:15:00 +1200

    29/04/2000 JSON doesn't work

--- Javascript.js
+++ Javascript.js
@@ -1,14 +1,73 @@
-let myObj = {"myKey":"some kind of value"};
-console.log(myObj["myKey"]); //will show "some kind of value"
-
 function submitAlert() {
     alert("Thank you for your message.");
   }
 
-fetch('./data.json')
+
+  function heartFunc(x) {
+    x.classList.toggle("fa-heart-o");
+  }
+
+
+fetch('/data.json')
 .then(res => {
     return res.json();
 })
 .then(data => {
     console.log(data);
 });
+
+
+function RecentTable() {
+    var recentlyPlayed = [
+        {"Image":"Hotel Diablo", "Title:":"Candy (feat. Trippie Redd)", "Artist":"Machine Gun Kelly", "Last Listened:":"47 minutes ago"}, 
+        {"Image":"Music To Be Murdered By", "Title:":"Marsh", "Artist":"Eminem", "Last Listened:":"52 minutes ago"},
+        {"Image":"ADHD", "Title:":"Revenge", "Artist":"Joyner Lucas", "Last Listened:":"59 minutes ago"},
+        {"Image":"Funeral", "Title:":"Mama Mia","Artist":"Lil Wayne", "Last Listened:":"An hour ago"},
+        {"Image":"Music To Be Murdered By", "Title:":"You Gon' Learn (feat. Royce Da 5'9\" & White Gold)", "Artist":"Eminem", "Last Listened:":"An hour ago"},
+        {"Image":"Bezerk", "Title:":"Bezerk (feat.A$AP Ferg)", "Artist":"Big Sean", "Last Listened:":"An hour ago"},
+        {"Image":"Blame it on Baby", "Title:":"DROP (feat. A Boogie wit da Hoodie & London On Da Track)", "Artist":"DaBaby", "Last Listened:":"An hour ago"},
+        {"Image":"Blame it on Baby", "Title:":"ROCKSTAR (feat. Roddy Ricch)", "Artist":"DaBaby", "Last Listened:":"An hour ago"},
+        {"Image":"Only The Team", "Title:":"Only The Team (with Lil Mosey & Lil Tjay)", "Artist":"Rvssian", "Last Listened:":"An hour ago"},
+        {"Image":"Hotel Diablo", "Title:":"Glass House (feat. Naomi Wild)", "Artist":"Machine Gun Kelly", "Last Listened:":"An hour ago"}    
+    ]
+
+    // EXTRACT VALUE FOR HTML HEADER. 
+    // ('Book ID', 'Book Name', 'Category' and 'Price')
+    var col = [];
+    for (var i = 0; i < recentlyPlayed.length; i++) {
+        for (var key in recentlyPlayed[i]) {
+            if (col.indexOf(key) === -1) {
+                col.push(key);
+            }
+        }
+    }
+
+    // CREATE DYNAMIC TABLE.
+    var table = document.createElement("table");
+
+    // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+
+    var tr = table.insertRow(-1);                   // TABLE ROW.
+
+    for (var i = 0; i < col.length; i++) {
+        var th = document.createElement("th");      // TABLE HEADER.
+        th.innerHTML = col[i];
+        tr.appendChild(th);
+    }
+
+    // ADD JSON DATA TO THE TABLE AS ROWS.
+    for (var i = 0; i < recentlyPlayed.length; i++) {
+
+        tr = table.insertRow(-1);
+
+        for (var j = 0; j < col.length; j++) {
+            var tabCell = tr.insertCell(-1);
+            tabCell.innerHTML = recentlyPlayed[i][col[j]];
+        }
+    }
+
+    // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+    var divContainer = document.getElementById("showData");
+    divContainer.innerHTML = "";
+    divContainer.appendChild(table);
+}
