f816cc3e99981b98586c3844b376b6b7640704b8
WILLCJ8 <50220258+WILLCJ8@users.noreply.github.com>
2020-05-01 00:01:35 +1200

    Added updated website, JSON now is placed into the page - need to fix heart function and add two more tables and comment system, als oto fix heart function.

--- Javascript.js
+++ Javascript.js
@@ -4,63 +4,29 @@ function submitAlert() {
 
 
   function heartFunc(x) {
-    x.classList.toggle("fa-heart-o");
+    x.classList.toggle("fa-heart");
   }
 
 
-fetch('/data.json')
+
+const tableMain = document.getElementById('table');
+
+fetch('/Recently Played.json')
 .then(res => {
     return res.json();
 })
 .then(data => {
-    console.log(data);
+    let table = '';
+    table += "<table>"
+    data.forEach(element => {
+        table += '<tr>'
+        table += '<td><i onclick="heartFunc(this)" class="fa fa-heart-o"></i></td>'
+        table += `<td><img src="${element["Image"]}"></td>`
+        table += `<td>${element["Title"]}</td>`
+        table += `<td>${element["Artist"]}</td>`
+        table += `<td>${element["Last Listened"]}</td>`
+        table += "</tr>"
+    })
+    table += "</table>"
+    tableMain.innerHTML = table;
 });
-
-
-
-
-function RecentTable() {
-    var recentlyPlayed = [
-        {"Image":"Hotel Diablo", "Title:":"Candy (feat. Trippie Redd)", "Artist":"Machine Gun Kelly", "Last Listened:":"47 minutes ago"}, 
-        {"Image":"Music To Be Murdered By", "Title:":"Marsh", "Artist":"Eminem", "Last Listened:":"52 minutes ago"},
-        {"Image":"ADHD", "Title:":"Revenge", "Artist":"Joyner Lucas", "Last Listened:":"59 minutes ago"},
-        {"Image":"Funeral", "Title:":"Mama Mia","Artist":"Lil Wayne", "Last Listened:":"An hour ago"},
-        {"Image":"Music To Be Murdered By", "Title:":"You Gon' Learn (feat. Royce Da 5'9\" & White Gold)", "Artist":"Eminem", "Last Listened:":"An hour ago"},
-        {"Image":"Bezerk", "Title:":"Bezerk (feat.A$AP Ferg)", "Artist":"Big Sean", "Last Listened:":"An hour ago"},
-        {"Image":"Blame it on Baby", "Title:":"DROP (feat. A Boogie wit da Hoodie & London On Da Track)", "Artist":"DaBaby", "Last Listened:":"An hour ago"},
-        {"Image":"Blame it on Baby", "Title:":"ROCKSTAR (feat. Roddy Ricch)", "Artist":"DaBaby", "Last Listened:":"An hour ago"},
-        {"Image":"Only The Team", "Title:":"Only The Team (with Lil Mosey & Lil Tjay)", "Artist":"Rvssian", "Last Listened:":"An hour ago"},
-        {"Image":"Hotel Diablo", "Title:":"Glass House (feat. Naomi Wild)", "Artist":"Machine Gun Kelly", "Last Listened:":"An hour ago"}    
-    ]
-
-    var col = [];
-    for (var i = 0; i < recentlyPlayed.length; i++) {
-        for (var key in recentlyPlayed[i]) {
-            if (col.indexOf(key) === -1) {
-                col.push(key);
-            }
-        }
-    }
-
-    var table = document.createElement("table");
-
-    var tr = table.insertRow(-1);
-
-    for (var i = 0; i < col.length; i++) {
-        var th = document.createElement("th");
-        th.innerHTML = col[i];
-        tr.appendChild(th);
-    }
-    for (var i = 0; i < recentlyPlayed.length; i++) {
-
-        tr = table.insertRow(-1);
-
-        for (var j = 0; j < col.length; j++) {
-            var tabCell = tr.insertCell(-1);
-            tabCell.innerHTML = recentlyPlayed[i][col[j]];
-        }
-    }
-    var divContainer = document.getElementById("showData");
-    divContainer.innerHTML = "";
-    divContainer.appendChild(table);
-}
