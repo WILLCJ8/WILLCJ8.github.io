530f58fa6b7fe3bbf98d6f9a87bdb6afb3b9c77f
WILLCJ8 <50220258+WILLCJ8@users.noreply.github.com>
2020-05-01 15:22:39 +1200

    Added final website, few things couldn't be done/counldn't figure out. Mostly works I believe though.

--- Javascript.js
+++ Javascript.js
@@ -1,19 +1,23 @@
+//This is the function for the message system
 function submitAlert() {
     alert("Thank you for your message.");
   }
 
+  //This is the fuction for the heart/unheart system.
 function heartFunc(x) {
-    x.classList.toggle("fa-heart");
+    x.classList.toggle("fa-heart-o");
   }
 
 
-
+//This is where the "Recently Played" data and building is.
 const tableRecent = document.getElementById('Recently_Played');
 
-fetch('/Recently Played.json')
+//This chunk of code fetches the file needed.
+fetch('/Recently Played.json') 
 .then(res => {
     return res.json();
 })
+//This code below is where it is all outputted and placed into a table.
 .then(data => {
     let table = '';
     table += "<table>"
@@ -23,19 +27,22 @@ fetch('/Recently Played.json')
         table += `<td>&nbsp;&nbsp;${element["Title"]}&nbsp;&nbsp;</td>`
         table += `<td>&nbsp;&nbsp;${element["Artist"]}&nbsp;&nbsp;</td>`
         table += `<td>&nbsp;&nbsp;${element["Last Listened"]}</td>`
-        table += '<td>&nbsp;&nbsp;<i onclick="heartFunc(this)" class="fa fa-heart-o"></i>&nbsp;&nbsp;</td>'
+        table += '<td>&nbsp;&nbsp;<i onclick="heartFunc(this)" class="fa fa-heart"></i>&nbsp;&nbsp;</td>'
         table += "</tr>"
     })
     table += "</table>"
-    tableRecent.innerHTML = table;
+    tableRecent.innerHTML = table; //Outputs to html
 });
 
+//This is where the "Recently Played" data and building is.
 const tableTracks = document.getElementById('Top_Tracks');
 
+//This chunk of code fetches the file needed.
 fetch('/Top Tracks.json')
 .then(res => {
     return res.json();
 })
+//This code below is where it is all outputted and placed into a table.
 .then(data => {
     let table = '';
     table += "<table>"
@@ -45,19 +52,22 @@ fetch('/Top Tracks.json')
         table += `<td>&nbsp;&nbsp;${element["Title"]}&nbsp;&nbsp;</td>`
         table += `<td>&nbsp;&nbsp;${element["Artist"]}&nbsp;&nbsp;</td>`
         table += `<td>&nbsp;&nbsp;${element["Total Listens"]}</td>`
-        table += '<td>&nbsp;&nbsp;<i onclick="heartFunc(this)" class="fa fa-heart-o"></i>&nbsp;&nbsp;</td>'
+        table += '<td>&nbsp;&nbsp;<i onclick="heartFunc(this)" class="fa fa-heart"></i>&nbsp;&nbsp;</td>'
         table += "</tr>"
     })
     table += "</table>"
-    tableTracks.innerHTML = table;
+    tableTracks.innerHTML = table; //Outputs to html
 });
 
+//This is where the "Recently Played" data and building is.
 const gridArtists = document.getElementById('Top_Artists');
 
+//This chunk of code fetches the file needed.
 fetch('/Top Artists.json')
 .then(res => {
     return res.json();
 })
+//This code below is where it is all outputted and placed into a table.
 .then(data => {
     let table = '';
     table += "<table>"
@@ -65,5 +75,5 @@ fetch('/Top Artists.json')
         table += `<img src="${element["Image"]}" style="margin-left: 20px">&nbsp;&nbsp;`
     })
     table += "</table>"
-    gridArtists.innerHTML = table;
+    gridArtists.innerHTML = table; //Outputs to html
 });
